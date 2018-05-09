520e7bd709065992472d3f02ae62e6053d92c69c
Mori <leokim1022@snu.ac.kr>
2018-05-09 23:09:40 +0900

    Update tf_gan.py

--- 0509/tf_gan.py
+++ 0509/tf_gan.py
@@ -52,4 +52,60 @@ def discriminator( x , reuse = False):
     return output
 
 def random_noise(batch_size):
-return np.random.normal(size=[batch_size , random_size])
+    return np.random.normal(size=[batch_size , random_size])
+
+
+
+
+g = tf.Graph()
+
+with g.as_default():
+    X = tf.placeholder(tf.float32, [None, 784])
+    Z = tf.placeholder(tf.float32, [None, random_size])
+
+    fake_x = generator(Z)
+    result_of_fake = discriminator(fake_x)
+    result_of_real = discriminator(X , True)
+
+    g_loss = tf.reduce_mean( tf.log(result_of_fake) )
+    d_loss = tf.reduce_mean( tf.log(result_of_real) + tf.log(1 - result_of_fake) )
+
+    t_vars = tf.trainable_variables()
+    g_vars = [var for var in t_vars if "Gen" in var.name]
+    d_vars = [var for var in t_vars if "Dis" in var.name]
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    g_train = optimizer.minimize(-g_loss, var_list= g_vars)
+    d_train = optimizer.minimize(-d_loss, var_list = d_vars)
+
+
+
+with tf.Session(graph = g) as sess:
+    sess.run(tf.global_variables_initializer())
+    total_batchs = int(train_x.shape[0] / batch_size)
+
+    for epoch in range(total_epochs):
+        for batch in range(total_batchs):
+            batch_x = train_x[batch * batch_size : (batch+1) * batch_size]
+            batch_y = train_y[batch * batch_size : (batch+1) * batch_size]
+            noise = random_noise(batch_size)
+
+            sess.run(g_train , feed_dict = {Z : noise})
+            sess.run(d_train, feed_dict = {X : batch_x , Z : noise})
+
+            gl, dl = sess.run([g_loss, d_loss], feed_dict = {X : batch_x , Z : noise})
+
+
+        if epoch % 2 == 0:
+            print("======= Epoch : ", epoch , " =======")
+            print("generator: " , -gl )
+            print("discriminator: " , -dl )
+
+
+        samples = 20
+        if epoch % 2 == 0:
+            sample_noise = random_noise(samples)
+            gen = sess.run(fake_x , feed_dict = { Z : sample_noise})
+
+            for i in range(samples):
+                img = gen[i].reshape([28,28])
+                mpimg.imsave('./epoch/epoch'+str(epoch)+'_'+str(i)+'.png', img, format='png')
