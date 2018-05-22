4f9cb34b25a3a33a97660d32bf51d2f6b2b26410
Mori <leokim1022@snu.ac.kr>
2018-05-22 15:25:02 +0900

    Update shape_gen.py

--- 0522/shape_gen.py
+++ 0522/shape_gen.py
@@ -7,12 +7,13 @@ def ru():
 def rc():
     return (ru(), ru(), ru())
 
+tri_num = 0
+circle_num = 0
+rect_num = 0
+
 for i in range(1000):
     shape_type= random.randint(0,2)
     filename = ""
-    tri_num = 0
-    circle_num = 0
-    rect_num = 0
     if shape_type == 0:
         shape = plt.Polygon(([ru()/2, ru()],[ru()/2+0.5, ru()],[ru(), ru()]), color=rc())
         tri_num += 1
@@ -20,11 +21,11 @@ for i in range(1000):
     elif shape_type == 1:
         shape = plt.Circle((ru()/2+0.25, ru()/2+0.25), ru()/4, color=rc())
         circle_num += 1
-        filename = "circle_" + str(tri_num) + "_" + str(i+1) + ".png"
+        filename = "circle_" + str(circle_num) + "_" + str(i+1) + ".png"
     else:
         shape = plt.Rectangle((ru(), ru()), ru(), ru(), fc=rc())
         rect_num += 1
-        filename = "rectangle_" + str(tri_num) + "_" + str(i+1) + ".png"
+        filename = "rectangle_" + str(rect_num) + "_" + str(i+1) + ".png"
 
     fig, ax = plt.subplots()
     fig.set_size_inches(1,1)
